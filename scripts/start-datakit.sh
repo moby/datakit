1b46a7c5cc1bf24145ea316b83766c05a2831442
Thomas Gazagnaire <thomas@gazagnaire.org>
2016-04-05 14:54:39 +0100

    Add Dockerfile.github to build Datakit with Github bindings
    This is disabled by default to avoid making com.docker.db (shipped with Pinata)
    depend on libssl or libgmp.
    
    Signed-off-by: Thomas Gazagnaire <thomas@gazagnaire.org>

--- scripts/start-datakit.sh
+++ scripts/start-datakit.sh
@@ -2,9 +2,17 @@
 
 set -exu
 
+DATAKIT_GITHUB=${DATAKIT_GITHUB=0}
+
+if [ "$DATAKIT_GITHUB" != 0 ]; then
+    DOCKERFILE=Dockerfile.github
+else
+    DOCKERFILE=Dockerfile
+fi
+
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
-docker build -t datakit ${REPO_ROOT}
+docker build -t datakit -f ${DOCKERFILE} ${REPO_ROOT}
 docker rm -f db || echo skip
 docker run -p 5650:5640 --name=db --rm \
   -v ${HOME}/.github:/home/opam/.github \

febbc6c6e745bb60da1652dccaaa223d8c1690f1
Thomas Leonard <thomas.leonard@docker.com>
2016-04-25 10:55:23 +0100

    Updated CI script

--- ci/push.sh
+++ ci/push.sh
@@ -5,5 +5,5 @@ docker build -t $HOST/admin/datakit-ci .
 docker push $HOST/admin/datakit-ci
 ssh $SSH_LOGIN docker pull $HOST/admin/datakit-ci
 ssh $SSH_LOGIN docker rm -f datakit-ci
-ssh $SSH_LOGIN docker run -d --name datakit-ci -v /srv/datakit:/db -v /var/run/docker.sock:/var/run/docker.sock $HOST/admin/datakit-ci
+ssh $SSH_LOGIN docker run -d --name datakit-ci -v /srv/datakit:/srv/datakit -v /var/run/docker.sock:/var/run/docker.sock $HOST/admin/datakit-ci
 ssh $SSH_LOGIN docker attach --sig-proxy=false datakit-ci

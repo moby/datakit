94ff064d32137fef3952de05a3699cab2bfe0964
Thomas Gazagnaire <thomas@gazagnaire.org>
2016-07-08 10:57:11 +0200

    Fix Dockerfiles and dev start scripts
    - ./scripts/start-datakit.sh starts a Datakit server
    - ./scripts/start-datakit-gh-bridge.sh starts a GitHub bridge, connecting
      back to the Datakit Server and exposing the 9p interface previously
      exposed by datakit itself
    - ./scripts/start-client.sh starts a shell where the Datakit instance is
      mounted in `/db` and the GitHub bridge in `/gh`
    
    Signed-off-by: Thomas Gazagnaire <thomas@gazagnaire.org>

--- scripts/start-client.sh
+++ scripts/start-client.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
 
-docker run -it --privileged --rm --link datakit -v `pwd`:/src --entrypoint sh datakit
+CMD='datakit-mount -h 172.17.0.2 -p 5640 /db && \
+     datakit-mount -h 172.17.0.3 -p 5641 /gh && \
+     /bin/sh'
+
+docker run -it --privileged --rm \
+       --link datakit --link datakit-gh-bridge \
+       --entrypoint /bin/bash datakit -c "${CMD}"
--- scripts/start-datakit-gh-bridge.sh
+++ scripts/start-datakit-gh-bridge.sh
@@ -3,10 +3,13 @@
 set -exu
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
-DOCKERFILE=Dockerfile
-NAME=datakit
+DOCKERFILE=Dockerfile.github
+NAME=datakit-gh-bridge
 
 docker build -t ${NAME} -f ${DOCKERFILE} ${REPO_ROOT}
 
 docker rm -f ${NAME} || echo skip
-docker run --name=${NAME} --rm  ${NAME}
+docker run --name=${NAME} --rm \
+       -p 8080:80 \
+       -v ${HOME}/.github:/root/.github \
+       ${NAME}

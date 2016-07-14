7a968c83a12b4340e909d641819b86c69c61c734
Thomas Leonard <thomas.leonard@docker.com>
2016-07-14 12:23:54 +0100

    Don't log at info level every time we read or write a file
    Signed-off-by: Thomas Leonard <thomas.leonard@docker.com>

--- src/irmin-io/ir_io.ml
+++ src/irmin-io/ir_io.ml
@@ -199,7 +199,7 @@ module FS = struct
     mkdir dir >>= fun () ->
     let tmp = Filename.temp_file ?temp_dir (Filename.basename file) "write" in
     Lwt_pool.use openfile_pool (fun () ->
-        Log.info (fun f -> f "Writing %s (%s)" file tmp);
+        Log.debug (fun f -> f "Writing %s (%s)" file tmp);
         Lwt_unix.(openfile tmp [O_WRONLY; O_NONBLOCK; O_CREAT; O_TRUNC] 0o644)
         >>= fun fd ->
         Lwt.finalize (fun () -> protect fn fd) (fun () -> Lwt_unix.close fd)
@@ -240,7 +240,7 @@ module FS = struct
   let read_file file =
     Unix.handle_unix_error (fun () ->
         Lwt_pool.use openfile_pool (fun () ->
-            Log.info (fun l -> l "Reading %s" file);
+            Log.debug (fun l -> l "Reading %s" file);
             Lwt_unix.stat file >>= fun stats ->
             let size = stats.Lwt_unix.st_size in
             if size >= mmap_threshold then read_file_with_mmap file

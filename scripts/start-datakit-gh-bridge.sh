baeb42369c8ffe2afff1e1707bd3ed034a2e4d95
Thomas Gazagnaire <thomas@gazagnaire.org>
2016-07-16 22:50:12 +0100

    Improve dev scripts
    Signed-off-by: Thomas Gazagnaire <thomas@gazagnaire.org>

--- scripts/start-datakit-gh-bridge.sh
+++ scripts/start-datakit-gh-bridge.sh
@@ -5,11 +5,12 @@ set -exu
 REPO_ROOT=$(git rev-parse --show-toplevel)
 DOCKERFILE=Dockerfile.github
 NAME=datakit-gh-bridge
+ARGS="--listen=tcp://0.0.0.0:5641 -vv --datakit=tcp:192.168.65.1:5640"
 
 docker build -t ${NAME} -f ${DOCKERFILE} ${REPO_ROOT}
 
 docker rm -f ${NAME} || echo skip
 docker run --name=${NAME} --rm \
-       -p 8080:80 \
+       -p 8080:80 -p 5641:5641 \
        -v ${HOME}/.github:/root/.github \
-       ${NAME}
+       ${NAME} ${ARGS}
--- scripts/start-datakit.sh
+++ scripts/start-datakit.sh
@@ -5,8 +5,10 @@ set -exu
 REPO_ROOT=$(git rev-parse --show-toplevel)
 DOCKERFILE=Dockerfile
 NAME=datakit
+DATA=/tmp/datakit
+ARGS="--url=tcp://0.0.0.0:5640 --git=/data -vv"
 
 docker build -t ${NAME} -f ${DOCKERFILE} ${REPO_ROOT}
 
 docker rm -f ${NAME} || echo skip
-docker run --name=${NAME} --rm  ${NAME}
+docker run --name=${NAME} -v ${DATA}:/data -p 5640:5640 --rm  ${NAME} ${ARGS}

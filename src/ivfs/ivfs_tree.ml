c6f5d9dcd5508b928a97cbdf694b3297d3b7828a
Thomas Gazagnaire <thomas@gazagnaire.org>
2016-08-11 20:10:58 +0100

    minor: fix style of ivfs/ivfs_tree.ml[,i]
    Signed-off-by: Thomas Gazagnaire <thomas@gazagnaire.org>

--- src/ivfs/ivfs_tree.ml
+++ src/ivfs/ivfs_tree.ml
@@ -20,36 +20,39 @@ module type STORE = Irmin.S
 module type S = sig
   type repo
   type store
-  module File : sig
+  module File: sig
     type t
     type hash
-    val of_data : repo -> Ivfs_blob.t -> t
-    val hash : t -> hash Lwt.t
-    val content : t -> Ivfs_blob.t Lwt.t
-    val equal : t -> t -> bool
-    val pp_hash : Format.formatter -> hash -> unit
-    val size : t -> int64 Lwt.t
+    val of_data: repo -> Ivfs_blob.t -> t
+    val hash: t -> hash Lwt.t
+    val content: t -> Ivfs_blob.t Lwt.t
+    val equal: t -> t -> bool
+    val pp_hash: Format.formatter -> hash -> unit
+    val size: t -> int64 Lwt.t
   end
-  module Dir : sig
+  module Dir: sig
     type t
     type hash
-    val empty : repo -> t
-    val ty : t -> step -> [`File | `Directory | `None] Lwt.t
-    val lookup : t -> step -> [`File of File.t * perm | `Directory of t | `None ] Lwt.t
-    val lookup_path : t -> path -> [`File of File.t * perm | `Directory of t | `None ] Lwt.t
-    val get : t -> path -> t option Lwt.t
-    val map : t -> [`File of File.t * perm | `Directory of t] String.Map.t Lwt.t
-    val ls : t -> ([`File | `Directory] * step) list Lwt.t
-    val of_hash : repo -> hash -> t
-    val hash : t -> hash Lwt.t
-    val repo : t -> repo
-    val with_child : t -> step -> [`File of File.t * perm | `Directory of t] -> t Lwt.t
-    val without_child : t -> step -> t Lwt.t
+    val empty: repo -> t
+    val ty: t -> step -> [`File | `Directory | `None] Lwt.t
+    val lookup:
+      t -> step -> [`File of File.t * perm | `Directory of t | `None ] Lwt.t
+    val lookup_path:
+      t -> path -> [`File of File.t * perm | `Directory of t | `None ] Lwt.t
+    val get: t -> path -> t option Lwt.t
+    val map: t -> [`File of File.t * perm | `Directory of t] String.Map.t Lwt.t
+    val ls: t -> ([`File | `Directory] * step) list Lwt.t
+    val of_hash: repo -> hash -> t
+    val hash: t -> hash Lwt.t
+    val repo: t -> repo
+    val with_child:
+      t -> step -> [`File of File.t * perm | `Directory of t] -> t Lwt.t
+    val without_child: t -> step -> t Lwt.t
   end
-  val snapshot : store -> Dir.t Lwt.t
+  val snapshot: store -> Dir.t Lwt.t
 end
 
-module Make (Store : STORE) = struct
+module Make (Store: STORE) = struct
   type store = Store.t
   type repo = Store.Repo.t
 
@@ -63,8 +66,8 @@ module Make (Store : STORE) = struct
       | Hash of Store.Private.Contents.key
 
     type t = {
-      repo : Store.Repo.t;
-      mutable value : value;
+      repo: Store.Repo.t;
+      mutable value: value;
     }
 
     let of_data repo value =
@@ -77,13 +80,14 @@ module Make (Store : STORE) = struct
       match f.value with
       | Hash h -> Lwt.return h
       | Blob b ->
-          let contents_t = Store.Private.Repo.contents_t f.repo in
-          let data = Ivfs_blob.to_string b in
-          Store.Private.Contents.add contents_t data >|= fun hash ->
-          f.value <- Hash hash;
-          hash
+        let contents_t = Store.Private.Repo.contents_t f.repo in
+        let data = Ivfs_blob.to_string b in
+        Store.Private.Contents.add contents_t data >|= fun hash ->
+        f.value <- Hash hash;
+        hash
 
-    let digest_blob b = Store.Private.Contents.Key.digest (Ivfs_blob.to_ro_cstruct b)
+    let digest_blob b =
+      Store.Private.Contents.Key.digest (Ivfs_blob.to_ro_cstruct b)
 
     let equal a b =
       match a.value, b.value with
@@ -96,9 +100,9 @@ module Make (Store : STORE) = struct
       match f.value with
       | Blob b -> Lwt.return b
       | Hash h ->
-          let contents_t = Store.Private.Repo.contents_t f.repo in
-          Store.Private.Contents.read_exn contents_t h >|= fun data ->
-          Ivfs_blob.of_string data
+        let contents_t = Store.Private.Repo.contents_t f.repo in
+        Store.Private.Contents.read_exn contents_t h >|= fun data ->
+        Ivfs_blob.of_string data
 
     let size f =
       (* TODO: provide a more efficient API in Irmin to get sizes *)
@@ -118,8 +122,8 @@ module Make (Store : STORE) = struct
       | Map_only of map
 
     and t = {
-      repo : Store.Repo.t;
-      mutable value : value;
+      repo: Store.Repo.t;
+      mutable value: value;
     }
 
     let empty repo = {repo; value = Map_only String.Map.empty}
@@ -133,13 +137,14 @@ module Make (Store : STORE) = struct
       let map = lazy (
         let node_t = Store.Private.Repo.node_t repo in
         Store.Private.Node.read_exn node_t hash >|= fun node ->
-        Store.Private.Node.Val.alist node |> List.fold_left (fun acc (name, item) ->
-          acc |> String.Map.add name (
-            match item with
-            | `Contents (h, perm) -> `File (File.of_hash repo h, perm)
-            | `Node h -> `Directory (of_hash repo h)
-          )
-        ) String.Map.empty
+        Store.Private.Node.Val.alist node
+        |> List.fold_left (fun acc (name, item) ->
+            acc |> String.Map.add name (
+              match item with
+              | `Contents (h, perm) -> `File (File.of_hash repo h, perm)
+              | `Node h -> `Directory (of_hash repo h)
+            )
+          ) String.Map.empty
       ) in
       { repo; value = Hash (hash, map) }
 
@@ -159,10 +164,10 @@ module Make (Store : STORE) = struct
     let rec lookup_path dir = function
       | [] -> Lwt.return (`Directory dir)
       | p::ps ->
-          lookup dir p >>= function
-          | `Directory d -> lookup_path d ps
-          | `File _ as x when ps = [] -> Lwt.return x
-          | `None | `File _ -> Lwt.return `None
+        lookup dir p >>= function
+        | `Directory d -> lookup_path d ps
+        | `File _ as x when ps = [] -> Lwt.return x
+        | `None | `File _ -> Lwt.return `None
 
     let get dir path =
       lookup_path dir path >|= function
@@ -174,48 +179,32 @@ module Make (Store : STORE) = struct
       map dir >|= fun m ->
       String.Map.bindings m
       |> List.map (fun (name, value) ->
-        match value with
-        | `File _ -> `File, name
-        | `Directory _ -> `Directory, name
-      )
+          match value with
+          | `File _ -> `File, name
+          | `Directory _ -> `Directory, name
+        )
 
     let rec hash t =
       match t.value with
       | Hash (h, _) -> Lwt.return h
       | Map_only m ->
-          String.Map.bindings m
-          |> Lwt_list.fold_left_s (fun acc item ->
+        String.Map.bindings m
+        |> Lwt_list.fold_left_s (fun acc item ->
             match item with
-            | name, `File (f, perm) -> File.hash f >|= fun h -> (name, `Contents (h, perm)) :: acc
+            | name, `File (f, perm) ->
+              File.hash f >|= fun h -> (name, `Contents (h, perm)) :: acc
             | name, `Directory d ->
-                map d >>= fun map ->
-                if String.Map.is_empty map then (
-                  (* Ignore empty directories *)
-                  Lwt.return acc
-                ) else (
-                  hash d >|= fun h ->
-                  (name, `Node h) :: acc
-                )
+              map d >>= fun map ->
+              if String.Map.is_empty map then (
+                (* Ignore empty directories *)
+                Lwt.return acc
+              ) else (
+                hash d >|= fun h ->
+                (name, `Node h) :: acc
+              )
           ) []
-          >|= Store.Private.Node.Val.create
-          >>= Store.Private.Node.add (Store.Private.Repo.node_t t.repo)
-
-(*
-    let rec equal a b =
-      match a.value, b.value with
-      | Map_only a, Map_only b -> String.Map.equal eq_item a b
-      | Hash (a, _), Hash (b, _) -> Store.Private.Node.Key.equal a b
-      | Map_only m, Hash (a, _)
-      | Hash (a, _), Map_only m ->
-          let b = export m |> Store.Private.Node.Key.digest in
-          Store.Private.Node.Key.equal a b
-    and eq_item a b =
-      match a, b with
-      | `File a, `File b -> File.equal a b
-      | `Directory a, `Directory b -> equal a b
-      | `File _, `Directory _
-      | `Directory _, `File _ -> false
-*)
+        >|= Store.Private.Node.Val.create
+        >>= Store.Private.Node.add (Store.Private.Repo.node_t t.repo)
 
     let repo a = a.repo
 
--- src/ivfs/ivfs_tree.mli
+++ src/ivfs/ivfs_tree.mli
@@ -1,7 +1,7 @@
 (** Expose Irmin internal trees (to be upstreamed).
 
     Irmin's public API exposes a key/value store, but we need to see
-    directory nodes too.  This module uses Irmin's private API to get
+    directory nodes too. This module uses Irmin's private API to get
     that information.
 *)
 
@@ -27,79 +27,85 @@ module type S = sig
   type repo
   (** The type for Irmin repositories. *)
 
-  module File : sig
+  module File: sig
+
     type t
     (** The type for file contents. *)
 
     type hash
     (** The type for file IDs (hashes). *)
 
-    val of_data : repo -> Ivfs_blob.t -> t
+    val of_data: repo -> Ivfs_blob.t -> t
     (** [of_data repo data] is a file containing [data], which may later
         be stored in [repo]. *)
 
-    val hash : t -> hash Lwt.t
-    (** [hash f] is the hash of the contents of [f]. If [f] is not yet in [repo], calling this
-        will cause it to be written. *)
+    val hash: t -> hash Lwt.t
+    (** [hash f] is the hash of the contents of [f]. If [f] is not yet
+        in [repo], calling this will cause it to be written. *)
 
-    val content : t -> Ivfs_blob.t Lwt.t
+    val content: t -> Ivfs_blob.t Lwt.t
 
-    val equal : t -> t -> bool
+    val equal: t -> t -> bool
     (** Quick equality check (compares hashes). *)
 
-    val pp_hash : Format.formatter -> hash -> unit
+    val pp_hash: Format.formatter -> hash -> unit
 
-    val size : t -> int64 Lwt.t
+    val size: t -> int64 Lwt.t
   end
 
-  module Dir : sig
+  module Dir: sig
+
     type t
-    (** An immutable directory node.
-        Note: normally we treat an empty directory as an error, but we do allow it
-        for the root. *)
+    (** An immutable directory node. Note: normally we treat an empty
+        directory as an error, but we do allow it for the root. *)
 
     type hash
     (** The type for directory IDs (hashes). *)
 
-    val empty : repo -> t
+    val empty: repo -> t
 
-    val ty : t -> step -> [`File | `Directory | `None] Lwt.t
+    val ty: t -> step -> [`File | `Directory | `None] Lwt.t
     (** Check the type of a path. *)
 
-    val lookup : t -> step -> [`File of File.t * perm | `Directory of t | `None ] Lwt.t
+    val lookup:
+      t -> step -> [`File of File.t * perm | `Directory of t | `None ] Lwt.t
     (** Look up an immediate child by name. *)
 
-    val lookup_path : t -> path -> [`File of File.t * perm | `Directory of t | `None ] Lwt.t
+    val lookup_path:
+      t -> path -> [`File of File.t * perm | `Directory of t | `None ] Lwt.t
     (** Look up an item by path. *)
 
-    val get : t -> path -> t option Lwt.t
+    val get: t -> path -> t option Lwt.t
     (** Look up a sub-directory node. *)
 
-    val map : t -> [`File of File.t * perm | `Directory of t] String.Map.t Lwt.t
+    val map: t -> [`File of File.t * perm | `Directory of t] String.Map.t Lwt.t
     (** The contents of the directory. *)
 
-    val ls : t -> ([`File | `Directory] * step) list Lwt.t
+    val ls: t -> ([`File | `Directory] * step) list Lwt.t
     (** List the contents of a directory with the type of each item. *)
 
-    val of_hash : repo -> hash -> t
+    val of_hash: repo -> hash -> t
     (** [of_hash repo h] is the directory whose hash is [h] in [repo]. *)
 
-    val hash : t -> hash Lwt.t
-    (** [hash dir] is [dir]'s hash. This writes [dir] to the repository if it's not
-        yet stored. *)
+    val hash: t -> hash Lwt.t
+    (** [hash dir] is [dir]'s hash. This writes [dir] to the
+        repository if it's not yet stored. *)
 
-    val repo : t -> repo
+    val repo: t -> repo
     (** [repo dir] is the repository in which [dir] lives. *)
 
-    val with_child : t -> step -> [`File of File.t * perm | `Directory of t] -> t Lwt.t
-    (** [with_child dir name child] is a copy of [dir] except that [name] links to [child]. *)
+    val with_child:
+      t -> step -> [`File of File.t * perm | `Directory of t] -> t Lwt.t
+    (** [with_child dir name child] is a copy of [dir] except that
+        [name] links to [child]. *)
 
-    val without_child : t -> step -> t Lwt.t
-    (** [without_child dir name] is a copy of [dir] except that it has no child called [name]. *)
+    val without_child: t -> step -> t Lwt.t
+    (** [without_child dir name] is a copy of [dir] except that it has
+        no child called [name]. *)
 
   end
 
-  val snapshot : store -> Dir.t Lwt.t
+  val snapshot: store -> Dir.t Lwt.t
   (** Convert a branch, which may update while we read it, to a fixed directory
       by reading its current root. *)
 end

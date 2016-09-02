51b9edbc4a74ca935efc0431652c1305982690fd
Thomas Gazagnaire <thomas@gazagnaire.org>
2016-09-02 17:57:58 +0100

    github: fix the webhook server
    Signed-off-by: Thomas Gazagnaire <thomas@gazagnaire.org>

--- bridge/github/src/datakit_github_webhook.mli
+++ bridge/github/src/datakit_github_webhook.mli
@@ -7,6 +7,6 @@ val create: Github.Token.t -> Uri.t -> t
 val run: t -> unit Lwt.t
 val repos: t -> Repo.Set.t
 val watch: t -> Repo.t -> unit Lwt.t
-val events: t -> Github_t.event list
+val events: t -> (Repo.t * Github_t.event_constr) list
 val clear: t -> unit
 val wait: t -> unit Lwt.t

moby/datakit

commit d1fc7b77eaff07da4bd63281ec51db5dfa0c250c

ci: use `string list` instead of Datakit_path.t for reference names
The validation is still mostly done and will be done better in subsequent
commits where we switch to use Datakit_github instead of CI_githubg_hooks.

Signed-off-by: Thomas Gazagnaire <thomas@gazagnaire.org>

ci/src/cI_web_templates.ml
@@ -53,9 +53,9 @@ let gh_target_url = function
   | `Ref r ->
     let { Repo.user; repo } = CI_github_hooks.Ref.repo r in
     let id = CI_github_hooks.Ref.name r in
-    match Datakit_path.unwrap id with
+    match id with
     | "tags" :: id -> Fmt.strf "https://github.com/%s/%s/releases/tag/%s" user repo (String.concat ~sep:"/" id)
-    | _            -> Fmt.strf "https://github.com/%s/%s/tree/%a" user repo Datakit_path.pp id
+    | _            -> Fmt.strf "https://github.com/%s/%s/tree/%a" user repo CI_github_hooks.Ref.pp_name id
 
 let metadata_url t = function
   | `PR pr ->
@@ -66,7 +66,7 @@ let metadata_url t = function
     let { Repo.user; repo } = CI_github_hooks.Ref.repo r in
     let id = CI_github_hooks.Ref.name r in
     state_repo_url t "commits/github-metadata/%s/%s/ref/%a" user repo
-      Datakit_path.pp id
+      CI_github_hooks.Ref.pp_name id
 
 let commit_history_url t target =
   let { Repo.user; repo }, commit =
@@ -85,35 +85,35 @@ let pr_url { Repo.user; repo} pr =
   Printf.sprintf "/pr/%s/%s/%d" user repo pr
 
 let escape_ref path =
-  Uri.pct_encode ~scheme:"http" (String.concat ~sep:"/" (Datakit_path.unwrap path))
+  Uri.pct_encode ~scheme:"http" (String.concat ~sep:"/" path)
 
 let unescape_ref s =
-  Uri.pct_decode s |> Datakit_path.of_string_exn
+  Uri.pct_encode ~scheme:"http" s |> (fun s -> String.cuts ~sep:"/" s)
 
 let ref_url { Repo.user; repo} r =
-  Printf.sprintf "/ref/%s/%s/%s" user repo (escape_ref r)
+  Fmt.strf "/ref/%s/%s/%s" user repo (escape_ref r)
 
 let tag_map f map =
-  Datakit_path.Map.fold (fun key value acc ->
-      match Datakit_path.unwrap key with
+  CI_github_hooks.Ref.Index.fold (fun key value acc ->
+      match key with
       | "tags" :: _ -> [f key value] @ acc
       | _ -> acc
     ) map []
 
 let branch_map f map =
-  Datakit_path.Map.fold (fun key value acc ->
-      match Datakit_path.unwrap key with
+  CI_github_hooks.Ref.Index.fold (fun key value acc ->
+      match key with
       | "heads" :: _ -> [f key value] @ acc
       | _ -> acc
     ) map []
 
-let int_map f map =
-  CI_utils.IntMap.fold (fun key value acc ->
+let pr_map f map =
+  CI_github_hooks.PR.Index.fold (fun key value acc ->
       [f key value] @ acc
     ) map []
 
 let dash_map f map targets =
-  Datakit_path.Map.fold (fun key value acc ->
+  CI_github_hooks.Ref.Index.fold (fun key value acc ->
       match CI_target.ID_Set.mem (`Ref key) targets with
       | true -> [f key value] @ acc
       | false -> acc
@@ -186,8 +186,7 @@ let dashboard_widget id ref =
     | `Error -> "dashboard-error", "glyphicon-warning-sign", "Erroring", "OH NO! Something has gone terribly wrong"
     | `Failure -> "dashboard-failure", "glyphicon-remove", "Failing", "SOUND THE ALARM!!! The build has been broken!"
   in
-  let title =
-    match Datakit_path.unwrap id with
+  let title = match id with
     | ("heads" | "tags") :: tl -> tl
     | x -> x
   in
@@ -202,7 +201,7 @@ let ref_job ~project id ref =
   let jobs = CI_engine.jobs ref in
   let summary = summarise jobs in
   tr [
-    td [a ~a:[a_href (ref_url project id)] [pcdata (Fmt.to_to_string Datakit_path.pp id)]];
+    td [a ~a:[a_href (ref_url project id)] [pcdata (Fmt.to_to_string CI_github_hooks.Ref.pp_name id)]];
     td [status_list jobs];
     td [pcdata summary.CI_state.descr];
   ]
@@ -321,7 +320,7 @@ let pr_table (id, (prs, _)) =
     h2 [pcdata (Fmt.strf "PR status for %a" Repo.pp id)];
     table ~a:[a_class["table"; "table-bordered"; "table-hover"]] (
       tr [heading "PR"; heading "Title"; heading "State"; heading "Details"] ::
-      (int_map (pr_job ~project:id) prs)
+      (pr_map (pr_job ~project:id) prs)
     );
   ]
 
@@ -646,7 +645,7 @@ let job_row ~csrf_token ~page_url ~best_log job =
 
 let target_title = function
   | `PR pr -> Printf.sprintf "PR %d" (CI_github_hooks.PR.id pr)
-  | `Ref r -> Fmt.strf "Ref %a" Datakit_path.pp (CI_github_hooks.Ref.name r)
+  | `Ref r -> Fmt.strf "Ref %a" CI_github_hooks.Ref.pp_name (CI_github_hooks.Ref.name r)
 
 let target_commit = function
   | `PR pr -> CI_github_hooks.Commit.hash (CI_github_hooks.PR.head pr)
@@ -680,7 +679,7 @@ let target_page ~csrf_token ~target jobs t =
     match target with
     | `PR _ -> Nav.PRs
     | `Ref r ->
-      match CI_github_hooks.Ref.name r |> Datakit_path.unwrap with
+      match CI_github_hooks.Ref.name r with
       | "heads" :: _ -> Nav.Branches
       | "tags" :: _ -> Nav.Tags
       | _ -> assert false

ci/src/cI_config.ml
@@ -13,8 +13,7 @@ type t = {
   projects : project Repo.Map.t;
 }
 
-let id_of_branch name =
-  `Ref (Datakit_path.of_string_exn ("heads/" ^ name))
+let id_of_branch name = `Ref ("heads" :: String.cuts ~sep:"/" name)
 
 let project ~id ?(dashboards=["master"]) tests =
   let id = match Repo.of_string id with

ci/src/cI_engine.ml
@@ -64,10 +64,14 @@ let pp_job f j =
 
 let state job = snd job.state
 let git_target job = job.head
+
 let title pr =
   match pr.head with
   | `PR pr -> CI_github_hooks.PR.title pr
-  | `Ref r -> Fmt.strf "Branch %a" Datakit_path.pp (CI_github_hooks.Ref.name r)
+  | `Ref r ->
+    let open !CI_github_hooks in
+    Fmt.strf "Ref %a" Ref.pp_name (Ref.name r)
+
 let jobs pr = pr.jobs
 let job_name j = j.name
 let repo target = target.repo
@@ -75,8 +79,8 @@ let repo target = target.repo
 type project = {
   make_terms : CI_target.Full.t -> string CI_term.t String.Map.t;
   canaries : CI_target.ID_Set.t option;
-  mutable open_prs : target IntMap.t;
-  mutable refs : target Datakit_path.Map.t;
+  mutable open_prs : target CI_github_hooks.PR.Index.t;
+  mutable refs : target CI_github_hooks.Ref.Index.t;
 }
 
 type t = {
@@ -125,8 +129,8 @@ let create ~web_ui ?canaries connect_dk projects =
                  |> CI_utils.default CI_target.ID_Set.empty)
         in
         { make_terms;
-          open_prs = IntMap.empty;
-          refs = Datakit_path.Map.empty;
+          open_prs = CI_github_hooks.PR.Index.empty;
+          refs = CI_github_hooks.Ref.Index.empty;
           canaries }
       ) projects
   in
@@ -144,8 +148,7 @@ let gh_hooks t = t.db >|= fun db -> db.gh_hooks
 let targets t =
   t.projects |> Repo.Map.map (fun project -> project.open_prs, project.refs)
 
-let escape_ref path =
-  Uri.pct_encode ~scheme:"http" (String.concat ~sep:"/" (Datakit_path.unwrap path))
+let escape_ref path = List.map (fun p -> Uri.pct_encode ~scheme:"http" p) path
 
 let set_status t { Repo.user; repo } target name ~status ~descr =
   CI_prometheus.Counter.inc_one Metrics.status_updates;
@@ -158,8 +161,11 @@ let set_status t { Repo.user; repo } target name ~status ~descr =
       (commit, url)
     | `Ref r ->
       Log.info (fun f -> f "Job ref %a:%s -> %s" CI_github_hooks.Ref.dump r name descr);
-      let url = Uri.with_path t.web_ui (Printf.sprintf "ref/%s/%s/%s"
-                                          user repo (escape_ref (CI_github_hooks.Ref.name r))) in
+      let url =
+        let open !CI_github_hooks in
+        Uri.with_path t.web_ui
+          (Fmt.strf "ref/%s/%s/%a" user repo Ref.pp_name (escape_ref (Ref.name r)))
+      in
       let commit = CI_github_hooks.Ref.head r in
       (commit, url)
   in
@@ -255,20 +261,22 @@ let make_job ~parent name term =
   }
 
 let apply_canaries canaries prs refs =
+  let open !CI_github_hooks in
   match canaries with
   | None -> (prs, refs)
   | Some canaries ->
-    let prs = prs |> IntMap.filter (fun id _ -> CI_target.ID_Set.mem (`PR id) canaries) in
-    let refs = refs |> Datakit_path.Map.filter (fun id _ -> CI_target.ID_Set.mem (`Ref id) canaries) in
+    let prs = prs |> PR.Index.filter (fun id _ -> CI_target.ID_Set.mem (`PR id) canaries) in
+    let refs = refs |> Ref.Index.filter (fun id _ -> CI_target.ID_Set.mem (`Ref id) canaries) in
     (prs, refs)
 
 let listen ?switch t =
+  let open !CI_github_hooks in
   auto_restart t ?switch "monitor" @@ fun () ->
   Log.info (fun f -> f "Starting monitor loop");
   gh_hooks t >>= fun gh_hooks ->
   let check_pr ~snapshot ~repo project (id, pr) =
     Log.debug (fun f -> f "Checking for work on %a" CI_github_hooks.PR.dump pr);
-    begin match IntMap.find id project.open_prs with
+    begin match PR.Index.find id project.open_prs with
       | None ->
         let open_pr = {
           repo;
@@ -280,7 +288,7 @@ let listen ?switch t =
         |> Lwt_list.map_s (fun (name, term) -> make_job ~parent:open_pr name term)
         >>= fun jobs ->
         open_pr.jobs <- jobs;
-        project.open_prs <- IntMap.add id open_pr project.open_prs;
+        project.open_prs <- PR.Index.add id open_pr project.open_prs;
         Lwt.return open_pr
       | Some open_pr ->
         open_pr.head <- `PR pr; (* Update in all cases, because we read other things from the same snapshot.
@@ -291,8 +299,8 @@ let listen ?switch t =
   in
   let check_ref ~snapshot ~repo project (id, r) =
     Log.debug (fun f -> f "Checking for work on %a" CI_github_hooks.Ref.dump r);
-    begin match Datakit_path.Map.find id project.refs with
-      | exception Not_found ->
+    begin match Ref.Index.find id project.refs with
+      | None ->
         let target = {
           repo;
           head = `Ref r;
@@ -303,9 +311,9 @@ let listen ?switch t =
         |> Lwt_list.map_s (fun (name, term) -> make_job ~parent:target name term)
         >>= fun jobs ->
         target.jobs <- jobs;
-        project.refs <- Datakit_path.Map.add id target project.refs;
+        project.refs <- Ref.Index.add id target project.refs;
         Lwt.return target
-      | target ->
+      | Some target ->
         target.head <- `Ref r;
         Lwt.return target
     end >>= fun target ->
@@ -320,35 +328,36 @@ let listen ?switch t =
           let prs, refs = apply_canaries project.canaries prs refs in
           (* PRs *)
           let is_current id open_pr =
-            let current = IntMap.mem id prs in
+            let current = PR.Index.mem id prs in
             if not current then (
               Log.info (fun f -> f "Removing closed PR#%d" id);
               List.iter (fun j -> j.cancel ()) open_pr.jobs
             );
             current
           in
-          project.open_prs <- IntMap.filter is_current project.open_prs;
+          project.open_prs <- PR.Index.filter is_current project.open_prs;
           Lwt_mutex.with_lock t.term_lock (fun () ->
-              IntMap.bindings prs |> Lwt_list.iter_s (check_pr ~snapshot ~repo project)
+              PR.Index.bindings prs |> Lwt_list.iter_s (check_pr ~snapshot ~repo project)
             )
           >>= fun () ->
           (* Refs *)
           let is_current id target =
-            let current = Datakit_path.Map.mem id refs in
+            let current = Ref.Index.mem id refs in
             if not current then (
-              Log.info (fun f -> f "Removing closed branch %a" Datakit_path.pp id);
+              Log.info (fun f -> f "Removing closed branch %a" Ref.pp_name id);
               List.iter (fun j -> j.cancel ()) target.jobs
             );
             current
           in
-          project.refs <- Datakit_path.Map.filter is_current project.refs;
+          project.refs <- Ref.Index.filter is_current project.refs;
           Lwt_mutex.with_lock t.term_lock (fun () ->
-              Datakit_path.Map.bindings refs |> Lwt_list.iter_s (check_ref ~snapshot ~repo project)
+              Ref.Index.bindings refs |> Lwt_list.iter_s (check_ref ~snapshot ~repo project)
             )
         )
     )
 
 let rebuild t ~branch_name =
+  let open !CI_github_hooks in
   let jobs_needing_recalc = ref [] in
   let triggers = ref [] in
   let rec check_logs =
@@ -372,8 +381,8 @@ let rebuild t ~branch_name =
   in
   let check_target target = List.iter check_job target.jobs in
   t.projects |> Repo.Map.iter (fun _ project ->
-      project.open_prs |> IntMap.iter (fun _ x -> check_target x);
-      project.refs |> Datakit_path.Map.iter (fun _ x -> check_target x);
+      project.open_prs |> PR.Index.iter (fun _ x -> check_target x);
+      project.refs |> Ref.Index.iter (fun _ x -> check_target x);
     );
   match !triggers, !jobs_needing_recalc with
   | [], [] -> CI_utils.failf "No job depends on %S, so can't rebuild anything" branch_name

ci/src/cI_engine.mli
@@ -31,7 +31,9 @@ val dk : t -> DK.t Lwt.t
 (** [dk t] is the connection to DataKit. If not currently connected, this will be a sleeping
     thread that will resolve to the next successful connection. *)
 
-val targets : t -> (target IntMap.t * target Datakit_path.Map.t) Repo.Map.t
+val targets : t ->
+  (target CI_github_hooks.PR.Index.t * target CI_github_hooks.Ref.Index.t)
+    Repo.Map.t
 (** [targets t] is a snapshot of the current state of all known PRs and branches. *)
 
 val jobs : target -> job list

ci/src/cI_github_hooks.ml
@@ -1,3 +1,4 @@
+open Asetmap
 open Datakit_github
 
 let refs_dir = Datakit_path.of_string_exn "ref"
@@ -100,25 +101,34 @@ module PR = struct
     match compare a.id b.id with
     | 0 -> Commit.compare a.commit b.commit
     | r -> r
+
+  module Index = Map.Make(struct
+      type t = int
+      let compare (a:int) (b:int) = Pervasives.compare a b
+    end)
 end
 
 module Ref = struct
   type t = {
-    name : Datakit_path.t;              (* Relative to refs_dir *)
+    name : string list;              (* Relative to refs_dir *)
     head : Commit.t;
   }
 
   let repo t = Commit.repo t.head
   let name t = t.name
   let head t = t.head
-  let dump f t = Fmt.pf f "ref/%a (head=%a)"
-      Datakit_path.pp t.name
-      Commit.pp t.head
+  let pp_name ppf t = Fmt.string ppf (String.concat ~sep:"/" t)
+  let dump f t = Fmt.pf f "ref/%a (head=%a)" pp_name t.name Commit.pp t.head
 
   let compare a b =
-    match Datakit_path.compare a.name b.name with
+    match Pervasives.compare a.name b.name with
     | 0 -> Commit.compare a.head b.head
     | r -> r
+
+  module Index = Map.Make(struct
+      type t = string list
+      let compare = Pervasives.compare
+    end)
 end
 
 let connect gh = gh
@@ -180,19 +190,20 @@ let prs snapshot =
       | None -> Log.warn (fun f -> f "Invalid PR ID %S" id); Lwt.return acc
       | Some id -> pr snapshot id >|= function
         | None -> acc
-        | Some value -> IntMap.add id value acc
-    ) IntMap.empty
+        | Some value -> PR.Index.add id value acc
+    ) PR.Index.empty
 
 let refs snapshot =
   let open! Datakit_path.Infix in
-  let results = ref Datakit_path.Map.empty in
+  let results = ref Ref.Index.empty in
   let rec scan ~context leaf =
     let context = context / leaf in
     read_opt_file snapshot (refs_dir /@ context / "head") >>= function
     | Some head ->
       let hash = String.trim (Cstruct.to_string head) in
       let head = { Commit.snapshot; hash } in
-      results := Datakit_path.Map.add context { Ref.head; name = context } !results;
+      let name = Datakit_path.unwrap context in
+      results := Ref.Index.add name { Ref.head; name } !results;
       Lwt.return ()
     | None ->
       read_opt_dir snapshot (refs_dir /@ context) >>=
@@ -219,8 +230,7 @@ end
 module Snapshot = struct
   type t = {
     commit : DK.Commit.t;
-    mutable projects :
-      (PR.t CI_utils.IntMap.t * Ref.t Datakit_path.Map.t) Lwt.t Repo.Map.t;
+    mutable projects:  (PR.t PR.Index.t * Ref.t Ref.Index.t) Lwt.t Repo.Map.t;
   }
 
   let repo t r =
@@ -245,11 +255,11 @@ module Snapshot = struct
   let find id t =
     repo t (CI_target.Full.repo id) >|= fun (prs, refs) ->
     match CI_target.Full.id id with
-    | `PR pr -> IntMap.find pr prs >|?= fun x -> `PR x
+    | `PR pr -> PR.Index.find pr prs >|?= fun x -> `PR x
     | `Ref x ->
-      match Datakit_path.Map.find x refs with
-      | x -> Some (`Ref x)
-      | exception Not_found -> None
+      match Ref.Index.find x refs with
+      | Some x -> Some (`Ref x)
+      | None   -> None
 end
 
 let snapshot t =

ci/src/cI_github_hooks.mli
@@ -1,3 +1,4 @@
+open Asetmap
 open Datakit_github
 open CI_utils
 open! Result
@@ -37,16 +38,19 @@ module PR : sig
   val repo : t -> Repo.t
   val dump : t Fmt.t
   val compare : t -> t -> int
+  module Index: Map.S with type key = int
 end
 
 module Ref : sig
   type t
 
   val repo : t -> Repo.t
-  val name : t -> Datakit_path.t
+  val name : t -> string list
   val head : t -> Commit.t
   val dump : t Fmt.t
   val compare : t -> t -> int
+  val pp_name: string list Fmt.t
+  module Index: Map.S with type key = string list
 end
 
 val connect : DK.t -> t
@@ -66,8 +70,7 @@ end
 module Snapshot : sig
   type t
 
-  val repo :
-    t -> Repo.t -> (PR.t CI_utils.IntMap.t * Ref.t Datakit_path.Map.t) Lwt.t
+  val repo: t -> Repo.t -> (PR.t PR.Index.t * Ref.t Ref.Index.t) Lwt.t
   (** [repo snapshot r] is the state of the open PRs, branches and
       tags in [snapshot] for repository [r]. *)
 

ci/src/cI_target.ml
@@ -3,16 +3,16 @@ open Astring
 open !Asetmap
 
 module ID = struct
-  type t = [ `PR of int | `Ref of Datakit_path.t ]
+  type t = [ `PR of int | `Ref of string list ]
 
   let pp f = function
-    | `PR x -> Fmt.pf f "prs/%d" x
-    | `Ref x -> Datakit_path.pp f x
+    | `PR x  -> Fmt.pf f "prs/%d" x
+    | `Ref x -> Fmt.string f (String.concat ~sep:"/" x)
 
   let compare a b =
     match a, b with
-    | `PR a, `PR b -> compare a b
-    | `Ref a, `Ref b -> Datakit_path.compare a b
+    | `PR a, `PR b   -> compare a b
+    | `Ref a, `Ref b -> Pervasives.compare a b
     | _ -> compare a b
 end
 
@@ -38,9 +38,8 @@ module Full = struct
     let parse_target = function
       | ("heads" | "tags") as ref_type, ref ->
         let open! Datakit_path.Infix in
-        let ref_type = Datakit_path.of_steps_exn [ref_type] in
         begin match Datakit_path.of_string ref with
-          | Ok path -> `Ok (`Ref (ref_type /@ path))
+          | Ok path -> `Ok (`Ref (ref_type :: Datakit_path.unwrap path))
           | Error msg -> `Error msg
         end
       | "prs", id ->

ci/src/cI_target.mli
@@ -2,7 +2,7 @@ open Datakit_github
 open !Asetmap
 
 module ID : sig
-  type t = [ `PR of int | `Ref of Datakit_path.t ]
+  type t = [ `PR of int | `Ref of string list ]
   val pp : t Fmt.t
   val compare : t -> t -> int
 end

commit b6e2f29b41d147db508787b96bf453ba1648f6d3

ci: fix a regression when parsing reference in the webui
This was introduced by d1fc7b77

Signed-off-by: Thomas Gazagnaire <thomas@gazagnaire.org>

ci/src/cI_web_templates.ml
@@ -89,7 +89,7 @@ let escape_ref path =
   Uri.pct_encode ~scheme:"http" (String.concat ~sep:"/" path)
 
 let unescape_ref s =
-  Uri.pct_encode ~scheme:"http" s |> (fun s -> String.cuts ~sep:"/" s)
+  Uri.pct_decode s |> (fun s -> String.cuts ~sep:"/" s)
 
 let ref_url { Repo.user; repo} r =
   Fmt.strf "/ref/%s/%s/%s" user repo (escape_ref r)
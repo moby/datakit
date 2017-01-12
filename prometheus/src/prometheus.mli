cff2593272f4e0d510157f9a3f898eefa917a660
Thomas Leonard <thomas.leonard@docker.com>
2017-01-12 11:22:22 +0000

    prometheus: add some API documentation
    Signed-off-by: Thomas Leonard <thomas.leonard@docker.com>

--- prometheus/src/prometheus.mli
+++ prometheus/src/prometheus.mli
@@ -1,20 +1,62 @@
+(** Collect metrics for Prometheus.
+    See: https://prometheus.io/
+
+    Notes:
+
+    - The Prometheus docs require that client libraries are thread-safe. We interpret this to mean safe
+      with Lwt threads, NOT with native threading.
+
+    - The [time] functions are unfortunate, as they create a dependency on [Unix].
+      Maybe they should be moved elsewhere.
+
+    - This module automatically initialises itself and registers some standard collectors relating to
+      GC statistics, as recommended by Prometheus.
+
+    - The API is rather limited. In particular, it is not yet possible to write your own collectors.
+*)
+
 module CollectorRegistry : sig
   type t
+  (** A collection of metrics to be monitored. *)
+
   type snapshot
+  (** The result of reading all the metrics. *)
+
   val create : unit -> t
+  (** [create ()] is a fresh registry. This is mostly useful for testing. *)
+
   val default : t
+  (** The default registry. *)
+
   val collect : t -> snapshot
+  (** Read the current value of each metric. *)
 end
 
 module TextFormat_0_0_4 : sig
   val output : CollectorRegistry.snapshot Fmt.t
+  (** Format a snapshot in Prometheus's text format, version 0.0.4. *)
 end
 
 module type METRIC = sig
   type family
+  (** A collection of metrics that are the same except for their labels.
+      e.g. "Number of HTTP responses" *)
+
   type t
+  (** A particular metric.
+      e.g. "Number of HTTP responses with code=404" *)
+
   val v_labels : label_names:string array -> ?registry:CollectorRegistry.t -> help:string -> ?namespace:string -> ?subsystem:string -> string -> family
+  (** [v_labels ~label_names ~help ~namespace ~subsystem name] is a family of metrics with full name
+      [namespace_subsystem_name] and documentation string [help]. Each metric in the family will provide
+      a value for each of the labels.
+      The new family is registered with [registry] (default: [CollectorRegistry.default]). *)
+
   val labels : family -> string array -> t
+  (** [labels family label_values] is the metric in [family] with these values for the labels.
+      The order of the values must be the same as the order of the [label_names] passed to [v_labels];
+      you may wish to write a wrapper function with labelled arguments to avoid mistakes.
+      If this is called multiple times with the same set of values, the existing metric will be returned. *)
 
   val v_label : label_name:string -> ?registry:CollectorRegistry.t -> help:string -> ?namespace:string -> ?subsystem:string -> string -> (string -> t)
   (** [v_label] is a convenience wrapper around [v_labels] for the case where there is a single label.
@@ -25,6 +67,8 @@ module type METRIC = sig
 end
 
 module Counter : sig
+  (** A counter is a cumulative metric that represents a single numerical value that only ever goes up. *)
+
   include METRIC
   val inc_one : t -> unit
   val inc : t -> float -> unit
@@ -32,6 +76,8 @@ module Counter : sig
 end
 
 module Gauge : sig
+  (** A gauge is a metric that represents a single numerical value that can arbitrarily go up and down. *)
+
   include METRIC
 
   val inc_one : t -> unit
@@ -47,6 +93,9 @@ module Gauge : sig
 end
 
 module Summary : sig
+  (** A summary is a metric that records both the number of readings and their total.
+      This allows calculating the average. *)
+
   include METRIC
 
   val observe : t -> float -> unit

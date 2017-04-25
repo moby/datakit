aae94b8a98f7437a5a1290e27222b9d3bbd1d80c
Thomas Gazagnaire <thomas@gazagnaire.org>
2017-04-25 22:39:25 +0100

    bridge-local-git: Update thel local Git bridge to use irmin 1.0 as well
    Signed-off-by: Thomas Gazagnaire <thomas@gazagnaire.org>

--- bridge/local/sync.mli
+++ bridge/local/sync.mli
@@ -1,5 +1,5 @@
 module Make
-    (S : Irmin.S with type branch_id = string)
+    (S : Irmin.S with type branch = string)
     (DK : Datakit_S.CLIENT) : sig
   val run : DK.t -> (Datakit_github.Repo.t * S.Repo.t) list -> 'a Lwt.t
 end

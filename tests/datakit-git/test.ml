ebea0c206415aa6611f947631ee36c7bc42843c3
Thomas Gazagnaire <thomas@gazagnaire.org>
2017-05-18 14:11:14 -0700

    git client: fix warnings in tests
    Signed-off-by: Thomas Gazagnaire <thomas@gazagnaire.org>

--- tests/datakit-git/test.ml
+++ tests/datakit-git/test.ml
@@ -4,8 +4,6 @@ module Client = Datakit_client_git
 
 let test_db = "_build/test-git"
 
-let root_entries = ["branch"; "debug"; "snapshots"; "trees"; "remotes"]
-
 let run fn =
   Lwt_main.run begin
     Git_unix.FS.create ~root:test_db () >>= fun db ->

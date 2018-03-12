a81a9b6f1eab0575c6640d0fdb788aa4845c759c
Peter Garamvolgyi <peter.garamvolgyi@hotmail.com>
2018-03-12 14:41:43 +0100

    ci: add support for Docker parameter 'network'

--- ci/src/cI_docker.mli
+++ ci/src/cI_docker.mli
@@ -14,5 +14,5 @@ val build : t -> ?from:Image.t -> CI_git.commit -> Image.t CI_term.t
 type command
 val command :
   logs:CI_live_log.manager -> pool:CI_monitored_pool.t -> timeout:float ->
-  label:string -> ?entrypoint:string -> ?user:string -> string list -> command
+  label:string -> ?entrypoint:string -> ?user:string -> ?network:string -> string list -> command
 val run : command -> Image.t -> unit CI_term.t
